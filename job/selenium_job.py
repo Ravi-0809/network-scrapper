9b47d4cd814a159c2ae93bf0965195a70ce61038
Ravi Bharadwaj C <ravibharadwajc@gmail.com>
2024-11-03 13:44:26 +0530

    remove unused code

--- job/selenium_job.py
+++ job/selenium_job.py
@@ -52,22 +52,13 @@ def capture_network_calls_headless(url, username = None, password = None):
         driver.get(updated_url)
         time.sleep(5) # wait 5 sec for all network calls
         har_data = proxy.har
-
-        # logs = driver.get_log("performance")
-        # requests = [json.loads(x["message"])["message"]["params"] for x in logs if "Network.responseReceived" in json.loads(x["message"])["message"]["method"]]
-        # for param in requests:
-        #     resp = param["response"] if "response" in param else ""
-        #     url = resp["url"] if "url" in resp else ""
-        #     status = resp["status"] if "status" in resp else ""
-        #     if (url != ""):
-        #         d = {"url": url, "status": status, "type": param['type']}
-        #         network_calls.append(d)
     except Exception as e:
         print(e)
     finally:
         driver.quit()
         proxy.close()
         server.stop()
+
     driver.quit()
     proxy.close()
     server.stop()
@@ -97,41 +88,3 @@ if __name__ == "__main__":
     writeToMongoDb(url=args.url, raw_data=har_data)
     
     exit(0)
-
-
-# ---- ui testing, ignore ----
-def capture_network_calls_ui(url, username = None, password = None):
-    network_calls = []
-    options = Options()
-    options.add_argument("--no-sandbox")  # Necessary for running as root in Docker
-    options.add_argument("--disable-dev-shm-usage")  # Overcomes limited resource issues
-    options.add_argument("--disable-gpu")  # Disable GPU rendering (optional but useful in headless)
-    options.add_argument("--remote-debugging-port=9222")  # Avoids the DevTools port issue
-    options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
-    
-    service = Service(driver_path)
-    # driver = webdriver.Chrome(service=service, options=options)
-    driver = webdriver.Remote(
-        command_executor=selenium_url,
-        options=options
-    )
-    
-    try:
-        driver.get(url)
-
-        while (driver.title):
-            logs = driver.get_log("performance")
-            requests = [json.loads(x["message"])["message"]["params"] for x in logs if "Network.responseReceived" in json.loads(x["message"])["message"]["method"]]
-            for param in requests:
-                resp = param["response"] if "response" in param else ""
-                url = resp["url"] if "url" in resp else ""
-                status = resp["status"] if "status" in resp else ""
-                if (url != ""):
-                    d = {"url": url, "status": status, "type": param['type']}
-                    network_calls.append(d)
-    except Exception as e:
-        print(e)
-    finally:
-        driver.quit()
-    driver.quit()
-    return network_calls
